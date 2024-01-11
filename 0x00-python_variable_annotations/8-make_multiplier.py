8f64b9c15147416027a1dc5d504080330d9aaf4f
glackyy <othmanesticky@gmail.com>
2024-01-11 15:16:19 +0100

    8-make_multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,8 @@
-#\!/usr/bin/python3
+#!/usr/bin/env python3
+"""type-annotated function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Creating a multiplier function"""
+    return lambda x: x * multiplier
