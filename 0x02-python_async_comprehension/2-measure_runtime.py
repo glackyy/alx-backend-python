370ca05854f7179645ffac9f38057634a55ae32c
glackyy <othmanesticky@gmail.com>
2024-01-16 11:41:52 +0100

    measure runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ from importlib import import_module as using
 async_comprehension = using('1-async_comprehension').async_comprehension
 
 
-async def measue_runtime() -> float:
+async def measure_runtime() -> float:
     """Executing async_comprehension 4 times and measures
     the total exec time"""
     st_time = time.time()
