17be96fc0b43d5bca96693fdcc0ce4004e757a1f
glackyy <othmanesticky@gmail.com>
2024-01-16 11:10:44 +0100

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Async Coroutine"""
+from typing import List
+from importlib import import_module as using
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Creating a list of 10 numbers from a 10-number gen"""
+    return [number async for number in async_generator()]
