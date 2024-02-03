9c60dc76af5d1c430c79d37c46c7f80245976bb6
glackyy <othmanesticky@gmail.com>
2024-02-03 15:02:28 +0100

    test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -104,7 +104,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @parameterized.expand([
         ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
-        ({'license': {'key': "bsl-1.0"}}, "bsl-1.0", False),
+        ({'license': {'key': "bsl-1.0"}}, "bsd-3-clause", False),
     ])
     def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
         """Testing the has_license method"""
